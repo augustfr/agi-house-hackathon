086e1ef6b025955a0c0ad02f05ef2573bc04a37d
augustfr <68298953+augustfr@users.noreply.github.com>
2023-06-24 14:55:08 -0700

    add env to reddit script

--- reddit.py
+++ reddit.py
@@ -1,15 +1,19 @@
+from dotenv import load_dotenv
+import os
 import praw
-import time
+
+load_dotenv()
 
 reddit = praw.Reddit(
-    client_id="my_client_id",
-    client_secret="my_client_secret",
-    user_agent="my_user_agent",
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent=os.getenv("REDDIT_USER_AGENT"),
 )
 
+
 while True:
-    hot_posts = reddit.subreddit('all').hot(limit=10)
+    hot_posts = reddit.subreddit("all").hot(limit=10)
     for post in hot_posts:
         print(post.title)
-    
+
     time.sleep(600)  # sleep for 10 minutes
