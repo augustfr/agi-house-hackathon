05c2c04588e2e3a224989b04a7de159767c94b48
augustfr <68298953+augustfr@users.noreply.github.com>
2023-06-24 15:16:44 -0700

    use environs for reddit

--- reddit_api.py
+++ reddit_api.py
@@ -1,14 +1,15 @@
-from dotenv import load_dotenv
 import os
 import praw
 import time
+from environs import Env
 
-load_dotenv()
+env = Env()
+env.read_env()
 
 reddit = praw.Reddit(
-    client_id=os.getenv("REDDIT_CLIENT_ID"),
-    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-    user_agent=os.getenv("REDDIT_USER_AGENT"),
+    client_id=env("REDDIT_CLIENT_ID", ""),
+    client_secret=env("REDDIT_CLIENT_SECRET", ""),
+    user_agent=env("REDDIT_USER_AGENT", ""),
 )
 
 
