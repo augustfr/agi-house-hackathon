d648fe7033b5b527f68f4e99a37c13e63af860d2
Lucas Negritto <lucas.negritto@gmail.com>
2023-06-24 19:08:15 -0700

    new speech class

--- news_agents/speech/voice.py
+++ news_agents/speech/voice.py
@@ -1,60 +1,51 @@
 from queue import Queue
 from typing import List
-from elevenlabs import generate, play, stream
+from elevenlabs import generate, stream
 from elevenlabs import set_api_key
 from environs import Env
 env = Env()
 env.read_env()
 set_api_key(env("ELEVEN_API_KEY"))
 
+from threading import Thread
 
-class TextToSpeech:
-    def __init__(self, voices: List[str], stream: bool = False):
+class SpeechQueue:
+    def __init__(self):
         self.queue = Queue()
-        self.voices = voices
-        self.is_playing = False
-        self.stream = stream
+        self.playing = False
+        self.stop_flag = False
+        self.current_thread = None
 
-    def add_to_queue(self, text: str, voice_index: int):
-        voice = self.voices[voice_index]
+    def add_text(self, text: str, voice: str):
         self.queue.put((text, voice))
 
     def start(self):
-        if not self.is_playing:
-            self.is_playing = True
-            self._play_queue()
+        if not self.playing:
+            self.playing = True
+            self.stop_flag = False
+            self.current_thread = Thread(target=self._play_queue)
+            self.current_thread.start()
 
     def stop(self):
-        self.is_playing = False
+        self.stop_flag = True
+        if self.current_thread is not None:
+            self.current_thread.join()
+            self.current_thread = None
+        self.playing = False
 
     def _play_queue(self):
-        while self.is_playing and not self.queue.empty():
+        while not self.stop_flag and not self.queue.empty():
             text, voice = self.queue.get()
-
-            if self.stream:
-                audio_stream = generate(text=text, voice=voice, stream=True)
-                stream(audio_stream)
-            else:
-                audio = generate(text=text, voice=voice)
-                play(audio)
-
+            audio_stream = generate(text, voice=voice, stream=True)
+            stream(audio_stream)
             self.queue.task_done()
-
-        self.is_playing = False
-
-
-# voices_list = ["Arnold", "Bella"]
-# tts = TextToSpeech(voices_list, stream=True)
-
-# import time
+        self.playing = False
 
 
-# tts.add_to_queue("Good evening, I'm Sarah Chen. We have a lot on the agenda tonight, but first, the groundbreaking news that's gripping the globe. NASA has just announced a successful landing on Mars, setting a new chapter in human exploration of the Red Planet. This is absolutely insane in the membrane.", 0)
-# tts.add_to_queue("Absolutely, Sarah. It's a momentous occasion for all of humanity. We've been following this mission since its launch, with bated breath and anticipation, and it's truly an emotional moment to hear about the successful landing.", 1)
+# speech_queue = SpeechQueue()
+# speech_queue.add_text("Hello, this is Arnold speaking.", "Arnold")
+# speech_queue.add_text("Now, this is Bella.", "Bella")
 
-# # start counting time
-# start = time.time()
-# tts.start()  # This will play all queued items in order
-# # print time taken
-# print("Time taken: {} seconds".format(time.time() - start))
+# speech_queue.add_text("Sup man", "Bella")
+
 
