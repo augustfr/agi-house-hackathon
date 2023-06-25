2686b50ffe061690ef94e9f58ec47a0b93d0571e
Lucas Negritto <lucas.negritto@gmail.com>
2023-06-24 17:30:43 -0700

    readme plan

--- news_agents/rss.py
+++ news_agents/rss.py
@@ -1,15 +1,56 @@
 import feedparser
+import trafilatura
 
-# URL of the RSS Feed
-feed_url = "http://feeds.bbci.co.uk/news/rss.xml"
+class FeedReader:
+    def __init__(self, feed_url=None):
+        self.feed_url = feed_url
+        self.feed = None
 
-# Parse the feed
-feed = feedparser.parse(feed_url)
+    def set_feed(self, feed_url):
+        self.feed_url = feed_url
+        self.feed = None
 
-# Print each post
-for entry in feed.entries:
-    print("Title: ", entry.title)
-    print("Link: ", entry.link)
-    print("Published: ", entry.published)
-    print("Summary: ", entry.summary)
-    print("---" * 20)
+    def get_feed_entries(self):
+        if self.feed_url is None:
+            return {"error": "No feed URL set"}
+
+        try:
+            self.feed = feedparser.parse(self.feed_url)
+        except Exception as e:
+            return {"error": str(e)}
+
+        entries = []
+        for entry in self.feed.entries:
+            entries.append({
+                "Title": entry.title,
+                "Link": entry.link,
+                "Published": entry.published,
+                "Summary": entry.summary
+            })
+
+        return entries
+
+    def get_plain_text(self, url):
+        try:
+            downloaded = trafilatura.fetch_url(url)
+            return trafilatura.extract(downloaded)
+        except Exception as e:
+            return {"error": str(e)}
+
+# Create a new FeedReader instance and specify the feed URL
+reader = FeedReader("http://feeds.bbci.co.uk/news/rss.xml")
+
+# Get entries from the feed
+entries = reader.get_feed_entries()
+if 'error' not in entries:
+    # Get the plain text of the first article
+    plain_text = reader.get_plain_text(entries[0]['Link'])
+
+    # Check if an error occurred
+    if 'error' in plain_text:
+        print('An error occurred:', plain_text['error'])
+    else:
+        print(plain_text)
+else:
+    print('An error occurred:', entries['error'])
+
